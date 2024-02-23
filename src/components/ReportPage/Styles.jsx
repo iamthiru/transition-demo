ff2b36144ac95e04384dc6cb99a4c64999374e83
Thiru <thirumalaivasan.sundharesan@softsuave.com>
2024-02-23 18:46:44 +0530

    merge code from other branch

--- src/components/ReportPage/Styles.jsx
+++ src/components/ReportPage/Styles.jsx
@@ -1,46 +1,63 @@
+import { deepPurple } from "@mui/material/colors";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles(() => ({
   container: {
     width: "100%",
-    height: "calc(100vh - 50px)",
+    height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
+    backgroundColor: deepPurple[400],
+
   },
   image: {
     width: 80,
     height: 80,
     borderRadius: "10px",
+    backgroundColor: "#fff",
+    boxShadow: "0px 10px 8px rgba(0, 0, 0, 0.1)",
   },
   icoon: {
     position: "absolute",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: "100%",
-    height: "100%",
-    top: "0%",
-    transition: "top 1s ease-in-out",
+    zIndex: 1,
+    top: "50%",
+    transition: "top 0.5s ease-in-out",
   },
   chatbox: {
-    width: 400,
-    height: 400,
-    borderRadius: "20px",
-    position: "relative",
-    border: "2px solid",
     display: "grid",
     gridTemplateColumns: "1fr",
     gridTemplateRows: "50px 1fr 100px",
-    gridColumnGap: "0px",
     gridRowGap: "10px",
+
+    backgroundColor: "#D3CFE2",
+    width: '60%',
+    height: '80%',
+    padding: '30px',
+    borderRadius: '3rem',
+    boxShadow: '0px 0px 17px 1px #888888',
+    opacity: 0,
+    transition: "opacity 1s ease-in-out",
   },
-  chatboxOpen: {
-    opacity: 1,
+  openChatbox: {
+    opacity: 2,
   },
   transimageOpen: {
-    top: "-50%",
+    top: "5%",
+  },
+  chatboxContainer:{
+    height: '93%',
+    padding: '5px',
+    overflow: 'auto',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+    '-ms-overflow-style': 'none', 
+    'scrollbar-width': 'none',
   },
 }));
 
--- src/components/ReportPage/index.jsx
+++ src/components/ReportPage/index.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { Box, Typography } from '@mui/material';
-import useStyles from './Styles';
-import FormContainer from './FormContainer';
-import ProgressBar from './ProgressBar';
+import { useState } from "react";
+import { Box, Container } from "@mui/material";
+import useStyles from "./Styles";
+import FormContainer from "./FormContainer";
+import ProgressBar from "./ProgressBar";
 
 const ReportPage = () => {
   const classes = useStyles();
@@ -14,21 +14,81 @@ const ReportPage = () => {
 
   return (
     <Box className={classes.container}>
-      <Box className={classes.chatbox}>
+      <Box
+        className={`${classes.icoon} ${
+          isChatboxOpen ? classes.transimageOpen : ""
+        }`}
+      >
+        <img
+          className={`${classes.image}`}
+          src="https://via.placeholder.com/150.png"
+          alt="Image"
+          onClick={openChatbox}
+        />
+      </Box>
+      <Box
+        className={`${classes.chatbox} ${
+          isChatboxOpen ? classes.openChatbox : ""
+        }`}
+      >
         <ProgressBar />
-        <Box
-          className={`${classes.icoon} ${
-            isChatboxOpen ? classes.transimageOpen : ''
-          }`}
-        >
-          <img
-            className={`${classes.image}`}
-            src="https://via.placeholder.com/150.png"
-            alt="Image"
-            onClick={openChatbox}
-          />
+
+        <Box className={classes.chatboxContainer}>
+          <div
+            className={`${classes.fadeOverlay} ${classes.fadeOverlayTop}`}
+          ></div>
+          <Container disableGutters>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              veritatis commodi quos rem officiis reprehenderit facere rerum
+              enim esse sint? Ducimus nisi animi optio nemo tenetur et dolore
+              voluptatem incidunt.
+            </Box>
+          </Container>
         </Box>
-        <Typography variant="h6">Chatbox</Typography>
+
         <FormContainer />
       </Box>
     </Box>
